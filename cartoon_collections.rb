34106084c357264069646a4ba5f625c8c6a5d304
William Luck <luck.william.h@gmail.com>
2022-10-23 15:42:01 -0300

    commit tree c722e881e200cccc84b6bdd6b6df97554258b6cd

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,51 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+
+def roll_call_dwarves(array)
+  array.each_with_index do |element, index|
+    puts "#{index + 1}. #{element}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |word|
+    word.capitalize + '!'
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  filtered = array.select do |string|
+    string.length > 4
+  end
+
+  if filtered.length > 0
+    true
+  else
+    false
+  end
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+ingredients = ["garlic", "rosemary", "bread"]
+cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # This is great because the select method will return a new array of all the items that meet the true condition in include. It will either be an empty array or an array with all the items that are cheese. Result[0] will return the first match, or nil with an empty array. 
+  result = array.select do |item|
+    cheese_types.include?(item)
+  end
+
+  result[0]
+  
 end
+
+# binding.pry
+0
